e6ce16f6f82324fe9bdf7351506669453f0bc5d0
nakamura0209 <umenosuke828828@gmail.com>
2023-11-30 02:57:23 +0000

    feat: API実行時にセッションに保存した過去のやり取りを送付する処理を追加

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import os
+from urllib import response
 import openai
 import streamlit as st
 from langchain.chat_models import AzureChatOpenAI
@@ -9,10 +10,10 @@ class LangchainChatModelFactory:
     @staticmethod
     def create_instance():
         return AzureChatOpenAI(
-            openai_api_base="https://openai-test-gpt4-20231118.openai.azure.com/",  # type: ignore
-            openai_api_version="2023-07-01-preview",  # type: ignore
-            deployment_name="openai-test-gpt4",  # type: ignore
-            openai_api_key="547aae27000d4df79923b93736993d8a",  # type: ignore
+            openai_api_base="https://openai-test-gpt4-20231118.openai.azure.com/",  # type: ignore //pylance誤検知のため
+            openai_api_version="2023-07-01-preview",  # type: ignore //pylance誤検知のため
+            deployment_name="openai-test-gpt4",  # type: ignore //pylance誤検知のため
+            openai_api_key="547aae27000d4df79923b93736993d8a",  # type: ignore //pylance誤検知のため
             openai_api_type="azure",
             model_version="gpt-4",
             # tiktoken_model_name=os.environ.get("AZURE_OPENAI_TIKTOKEN_MODEL_NAME", ""),
@@ -24,15 +25,22 @@ def main():
     st.set_page_config(page_title="Stream-AI-Chat", page_icon="🤖")
     st.header("Stream-AI-Chat")
 
+    llm = LangchainChatModelFactory.create_instance()
+
     # チャット履歴の初期化
     if "messages" not in st.session_state:
         st.session_state.messages = [SystemMessage(content="")]
 
+    # ユーザ入力を監視
     if user_input := st.chat_input("Input Your Message..."):
-        # なにか入力されればここが実行される
-        st.write(user_input)
+        st.session_state.messages.append(HumanMessage(content=user_input))  # type: ignore //pylance誤検知のため
+        with st.spinner("Generating ChatGPT answers..."):
+            response = llm(st.session_state.messages)  # type: ignore //pylance誤検知のため
+        st.session_state.messages.append(AIMessage(content=response.content))  # type: ignore //pylance誤検知のため
 
-    llm = LangchainChatModelFactory.create_instance()
+    messages = st.session_state.get("messages", [])
+    for message in messages:
+        st.write(message)
 
 
 if __name__ == "__main__":
