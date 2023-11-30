c76d605207a12b852c8d52a73498dc427cc7b8b2
nakamura0209 <umenosuke828828@gmail.com>
2023-11-30 05:43:38 +0000

    style: 存在しないモデルの排除

--- data_source/openai_data_source.py
+++ data_source/openai_data_source.py
@@ -12,14 +12,6 @@ MODELS = {
         "top_k": 10,
         "deployment_name": os.getenv("GPT3_5_TURBO_MODEL_DEPLOYMENT_NAME"),
     },
-    "gpt-3.5-turbo-16k": {
-        "name": "gpt-3.5-turbo-16k",
-        "max_tokens": 16384,
-        "max_prompt_tokens": 15384,
-        "max_response_tokens": 1000,
-        "top_k": 40,
-        "deployment_name": os.getenv("GPT3_5_TURBO_16K_MODEL_DEPLOYMENT_NAME"),
-    },
     "gpt-4": {
         "name": "gpt-4",
         "max_tokens": 8192,
